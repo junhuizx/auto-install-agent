f9698563dba5fa10b961f94d8df4cb36abbaf320
zouyapeng <yapeng.zou@newtouch.cn>
2016-04-06 07:22:58 +0800

    new file:   .main.sh.swp 	modified:   hosts 	modified:   main.sh 	new file:   package/libtool-ltdl-2.4.2-20.el7.x86_64.rpm 	deleted:    package/package 	new file:   package/unixODBC-2.3.1-10.el7.x86_64.rpm 	new file:   package/zabbix-agent-3.0.1-1.el7.x86_64.rpm

--- main.sh
+++ main.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
 
+
+hosts=(`cat hosts | awk '{print $0}'| sort 2>/dev/null`)
+num=${#hosts[@]}
+
+function find_host_up()
+{
+    for ((i=0;i<$num;i++))
+    do                                   
+        status=`ping ${hosts[$i]} -c 1 -W 1 | grep -c ' 0% packet loss'`
+        if [ $status -eq 1 ];then
+            echo ${hosts[$i]}
+        fi
+    done
+}
+
+function find_host_down()
+{       
+    for ((i=0;i<$num;i++))
+    do
+        status=`ping ${hosts[$i]} -c 1 -W 1 | grep -c ' 0% packet loss'`
+        if [ $status -eq 0 ];then
+            echo ${hosts[$i]}
+        fi 
+    done                
+}
+
+hostsup=(`find_host_up`)
+hostsdown=(`find_host_down`)
+
+echo 'Down Hosts'
+for host in ${hostsdown[@]}
+do
+    echo $host
+done
+
+echo 'Up Hosts'
+for host in ${hostsup[@]}
+do
+    echo $host
+done
